11c7c196db1f6f17ecaeb4e488cc80f41cbdf581
Saurabh Davda <saurabhdavda9977@gmail.com>
2024-03-29 15:02:46 +0530

    whatsapp ui done

---
+++ src/data/chatdata.js
@@ -0,0 +1,44 @@
+const DATA = [
+    {
+        "id":1,
+        "name":'Elon',
+        "photos":require('../../assets/elon.jpg'),
+        "lastMessage": "Space X - Tesla - Bitcoin",
+        "time":"10:00 AM",
+        "totalUnread":2
+    },
+    {
+        "id":2,
+        "name":'Sam',
+        "photos":require('../../assets/sam.jpg'),
+        "lastMessage": "Open AI - DALL E - ChatGPT",
+        "time":"11:00 AM",
+        "totalUnread":0
+    },
+    {
+        "id":3,
+        "name":'Satya',
+        "photos":require('../../assets/satya.jpg'),
+        "lastMessage": "Copilot Bing",
+        "time":"9:00 AM",
+        "totalUnread":1
+    },
+    {
+        "id":4,
+        "name":'Mira',
+        "photos":require('../../assets/mira.png'),
+        "lastMessage": "Open AI CTO ",
+        "time":"8:00 AM",
+        "totalUnread":1
+    },
+    {
+        "id":5,
+        "name":'Brian',
+        "photos":require('../../assets/brian.jpeg'),
+        "lastMessage": "AirBNB ",
+        "time":"10:50 AM",
+        "totalUnread":3
+    }
+]
+
+export default DATA
